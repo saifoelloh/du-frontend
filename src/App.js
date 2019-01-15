84718df0309588c894aed7472f7e51659fd2f54d
saifoelloh <saifoelloh@gmail.com>
2019-01-15 18:37:58 +0700

    second update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Home from './components/main/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="root">
+        <Switch>
+          <Route path="/" exact="true" component={Home} />
+          <Route path="/register" exact="true" component={Home} />
+          <Route path="/user" component={Home} />
+          <Route path="/admin" exact="true" component={Home} />
+          <Route path="/admin/{id}" component={Home} />
+        </Switch>
       </div>
     );
   }
